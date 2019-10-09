e56309fddf8aa9cca9268c2688e3540d4973b86c
Jeff Hykin <jeff.hykin@gmail.com>
2019-10-08 21:36:11 -0500

    improve the color of the points

--- src/renderer/components/LandingPage/Draggable.vue
+++ src/renderer/components/LandingPage/Draggable.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="point" :style="{ left: `${this.newX}px`, top: `${this.newY}px`, cursor: dragging? 'move':'pointer', color }" @click="activateDrag">
-        <svg class="icon icon-cross"><use xlink:href="#icon-cross"></use></svg>
-        <symbol id="icon-cross" viewBox="0 0 32 32">
-        <title>cross</title>
-            <animate attributeName="fill" values="red;blue;red" dur="10s" repeatCount="indefinite" />
-            <path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path>
-        </symbol>
+        <svg width=100% height=100%>
+            <line x1="0" y1="0" x2="100%" y2="100%" :style="{strokeWidth, stroke: color}">
+                <rainbow-line-animation />
+            </line>
+            <line x1="100%" y1="0" x2="1" y2="100%" :style="{strokeWidth, stroke: color}">
+                <rainbow-line-animation />
+            </line>
+        </svg>
     </div>
 </template>
 <script>
+    import RainbowLineAnimation from './rainbow-line-animation'
     export default {
+        components: {
+            RainbowLineAnimation
+        },
         name:  "point",
         props: ['x', 'y', 'uniqueName'],
         data: function() {
@@ -17,19 +23,19 @@
                 newX: this.$props.x,
                 newY: this.$props.y,
                 dragging: false,
-                color: 'red'
+                color: '#ef5350',
+                strokeWidth: '5%',
             }
         },
         mounted: function() {
             this.newX = this.$props.x
             this.newY = this.$props.y
-            this.colors = [ '#ef5350', '#ec407a', '#ab47bc', '#7e57c2', '#5c6bc0', '#42a5f5', '#29b6f6', '#26c6da', '#26a69a', '#66bb6a', '#9ccc65', '#d4e157', '#ffee58', '#ffca28', '#ffa726', '#ff7043']
             this.colorIndex = 0
-            setInterval(() => {
-                this.colorIndex++
-                this.colorIndex = this.colorIndex % this.colors.length
-                this.color = this.colors[this.colorIndex]
-            }, 250);
+            // setInterval(() => {
+            //     this.colorIndex++
+            //     this.colorIndex = this.colorIndex % this.colors.length
+            //     this.color = this.colors[this.colorIndex]
+            // }, 250);
         },
         computed: {
         },
@@ -66,27 +72,16 @@
 </script>
 <style scoped>
     .point {
+        --size: 16px;
         transition: color 0.2s ease-in;
         cursor: move;
         position: absolute;
         transform: translate(-50%, -50%);
         /* background-color: red; */
-        height: fit-content;
-        width: fit-content;
+        height: var(--size);
+        width: var(--size);
         display: flex;
         align-items: center;
         justify-content: center;
     }
-    /* -----
-SVG Icons - svgicons.sparkk.fr
------ */
-
-.icon {
-  display: inline-block;
-  width: 1em;
-  height: 1em;
-  stroke-width: 0;
-  stroke: currentColor;
-  fill: currentColor;
-}
 </style>
