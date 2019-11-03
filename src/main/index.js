a2ffdd663d99bd3598f05c958b17363d80c2381c
Jeff Hykin <jeff.hykin@gmail.com>
2019-11-03 01:19:44 -0600

    - got pasting working in packaged app

--- src/main/index.js
+++ src/main/index.js
@@ -1,50 +1,55 @@
-import { app, BrowserWindow } from 'electron'
+import { app, Menu, shell, BrowserWindow } from "electron"
+import electron from "electron"
+import defaultMenu from "electron-default-menu"
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+if (process.env.NODE_ENV !== "development") {
+    global.__static = require("path")
+        .join(__dirname, "/static")
+        .replace(/\\/g, "\\\\")
 }
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const winURL = process.env.NODE_ENV === "development" ? `http://localhost:9080` : `file://${__dirname}/index.html`
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000,
-    webPreferences: {
-        webSecurity: false
-    }
-  })
+function createWindow() {
+    /**
+     * Initial window options
+     */
+    mainWindow = new BrowserWindow({
+        height: 563,
+        useContentSize: true,
+        width: 1000,
+        webPreferences: {
+            webSecurity: false,
+        },
+    })
 
-  mainWindow.loadURL(winURL)
+    // Add the default menu
+    Menu.setApplicationMenu(Menu.buildFromTemplate(defaultMenu(app, shell)))
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow.loadURL(winURL)
+
+    mainWindow.on("closed", () => {
+        mainWindow = null
+    })
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
 })
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 /**
