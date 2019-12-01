00158953608c9efbfd153ebc49a33bc95836600b
Jeff Hykin <jeff.hykin@gmail.com>
2019-12-01 15:00:20 -0600

    - improve setup

--- setup.rb
+++ setup.rb
@@ -1,12 +1,13 @@
 require 'atk_toolbox'
 
-if OS.is?("mac")
-    system "brew install node"
-    system "npm install"
-elsif OS.is?("windows")
-    system "scoop install nodejs"
-    system "npm install"
-elsif OS.is?("ubuntu")
-    system "sudo apt install nodejs"
-    system "npm install"
+if not Console.has_command("npm")
+    if OS.is?("mac")
+        system "brew install node"
+    elsif OS.is?("windows")
+        system "scoop install nodejs"
+    elsif OS.is?("ubuntu")
+        system "sudo apt install nodejs"
+    end
 end
+
+system "npm install"
