2eca0bbbbe68616ee5819e409906722b641e1ceb
Jeff Hykin <jeff.hykin@gmail.com>
2019-12-17 20:38:24 -0600

    add documentation

--- src/plugins/simple-functional-components.js
+++ src/plugins/simple-functional-components.js
@@ -1,5 +1,35 @@
 import Vue from 'vue'
 
+
+// if you have JSX enabled
+// importing this will enable you to make very consice components like this:
+// 
+//     // one-liner arrow function component
+//     let helloWorld = (h) => <div class=hello-world>Hello World</div>
+//     // wrap it with vue extend
+//     helloWorld = Vue.extend(helloWorld)
+// 
+//     // use in the same file as a vue component
+//     export default {
+//         components: { 
+//             helloWorld
+//         },
+//     }
+// 
+// NOTE: the `h` variable is a necessary first argument
+// 
+// here's another example:
+//     let miniComponent = Vue.extend(
+//         (h, {data, children}, {prop1, prop2, prop3}) => {
+//             if (prop1) {
+//                 return <div>{children}</div>
+//             } else {
+//                 return <button>{prop2}</button>
+//             }
+//         }
+//     )
+// 
+
 let copyFunc = (someFunction,theContext=this) => (...args) => someFunction.apply(theContext,args)
 
 let originalVueExend = copyFunc(Vue.extend, Vue)
